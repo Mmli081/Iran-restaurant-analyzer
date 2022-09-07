ab2a203d94569ef3b6d57390f3a43bf114e539ba
Mmli081 <mshafei181@gmail.com>
2022-09-07 10:23:24 +0430

    delete MySQLInterface.py, update requirements.txt

--- code/MySQLInterface.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-user  = 'user_group4'
-password = 'OnhAeVtaxYca_group4'
-host = '45.139.10.138:80'
-db = 'group4'
-engine  = create_engine(f'mysql+pymysql://{user}:{password}@{host}/{db}')
-s = engine.execute('select 1+1')
-print(s.first()[0])
