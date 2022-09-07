80b51e80075720d4114aeebee0cda4bbabe8abbf
sobhan <sobhangholami502@gmail.com>
2022-09-07 16:09:57 +0430

    complete and update functions

--- code/crawl_SQLinterface.py
+++ code/crawl_SQLinterface.py
@@ -11,8 +11,8 @@ def time_format():
 def prep_features():
     pass
 
-def insert_cafe():
-    att_cafe = ["name","city","province","price_class","work_start","work_end"]
+def insert_cafe(result):
+    att_cafe = ["name","city","province",'phone',"price_class","work_start","work_end"]
     for row in result:
         data = []
         for att in att_cafe:
@@ -20,8 +20,10 @@ def insert_cafe():
         data = tuple(data)
         db.insert('cafe',data)
 
-def insert_cafe_address():
+def insert_cafe_address(result):
     for row in result:
         db.insert('cafe_address',(row['address'],))
 
-
+# def insert_cafe_rating():
+#     att_rating = ["price_class","food_quality","service","cost_value","environment"]
+#     for row in result:
