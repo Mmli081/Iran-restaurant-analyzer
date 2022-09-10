dec24a1bc8687048ed418ecd6d090eb53dad9948
Mmli081 <mshafei181@gmail.com>
2022-09-10 15:50:01 +0430

    move filters, complete crawl_SQL interface

--- code/crawl_SQLinterface.py
+++ code/crawl_SQLinterface.py
@@ -1,18 +1,8 @@
 import json
-from operator import and_
-from CRUD import Cafe, CafeAddress, CafeFeatures, CafeRating,Database, set_table, and_
+from CRUD import Cafe, CafeAddress, CafeFeatures, CafeRating,Database, set_table
 from crawl import scrape
 import pandas as pd
-from datetime import datetime
 
-# result = json.load(scrape())
-# db = Database('127.0.0.1:3306','sobhan','$Gh9170392008','group4')
-
-# test
-import json
-with open("../data/crawlSample.json") as f:
-    result = json.load(f)
-db = Database('localhost','mml','$Mml09357528086','group4Local')
 
 # define some function to help inserting
 def time_format(time):
@@ -68,33 +58,13 @@ def insert_cafe_features(result):
         LastID+=1
     db.insert(obj_list)
 
-#filters
-def filter_by_city(city):
-    return set_table('cafe').city == city
-
-def filter_by_province(province):
-    return set_table('cafe').province == province
-
-# def filter_by_has_features(features: list):
-
-def filter_by_range_work_time(work_start: str, work_end="00:00"):
-    work_start = datetime.strptime(work_start, '%H:%M').time()
-    if work_end == "00:00":
-        return set_table("cafe").work_start >= work_start
-    work_end = datetime.strptime(work_end, '%H:%M').time()
-    return and_(set_table("cafe").work_start >= work_start,
-                set_table("cafe").work_end <= work_end)
-
-def filter_by_range_cost(_from: int=0, _to: int=5):
-    return and_(set_table('cafe').cost >= _from,
-                set_table('cafe').cost <= _to,)
-
-
 def read(tablename, filter=None, order=None, n=0):
     return pd.read_sql(*db.read(tablename, filter, order, n))
 
-
 if __name__ == "__main__":
+
+    db = Database('45.139.10.138:80','user_group4','OnhAeVtaxYca_group4','group4')
+    result = json.load(scrape())
     insert_cafe(result)
     insert_cafe_address(result)
     insert_cafe_features(result)
